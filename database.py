157e112100fe27996f42c6ae4317035073f145e9
strakhovdenya <strakhov.denya@gmail.com>
2026-04-02 22:20:46 +0200

    change DB

--- database.py
+++ database.py
@@ -2,12 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 # SQLITE3
-# engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
-# SQLALCHEMY_DATABASE_URL = 'sqlite:///todosapp.db'
+SQLALCHEMY_DATABASE_URL = 'sqlite:///todosapp.db'
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 
 # POSTGRESQL
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:Ghbrjk12@localhost/TodoApplicationDatabase'
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:Ghbrjk12@localhost/TodoApplicationDatabase'
+# engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
